10849834c36e393650e4b70f9d5880c45f5cf715
mikur <100271723+mmaciejewskiuwm@users.noreply.github.com>
2022-03-24 16:54:27 +0100

    Add files via upload

---
+++ lab5a.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    int a=20,b=1;
+//    int n=7;
+//    int *w;
+//    z326(n,&w);
+////    zamien(&a,&b);
+//    double*p = rezerwuj();
+//    *p = 10.25;
+//    printf("Wartosc: %f",*p);
+//    printf("%d",a);
+//    printf("%d",w);
+    return 0;
+}
+//3.2.4
+//void zamien(int*a, int*b)
+//{
+//    if(*b<*a)
+//    {
+//        int pom;
+//        pom=*a;
+//        *a=*b;
+//        *b=pom;
+//    }
+//}
+//3.2.6
+//void z326(int n, int*w)
+//{
+// *w=n;
+//}
+//3210
+//double* rezerwuj()
+//{
+//    return malloc(sizeof(double));
+//}
+//int z3212(int n)
