622f726ae131fd5d6d93a0264ac0a1c7cc3347ec
mikur <100271723+mmaciejewskiuwm@users.noreply.github.com>
2022-05-26 17:10:26 +0200

    Add files via upload

---
+++ String/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Napisz wskaźnikową wersję funkcji strcpyEven. Funkcja ta ma kopiować co drugi znak z napisu s do napisu t. Zadbaj o koniec napisu - NUL.
+char* strcpyEven(char *t, const char *s)
+{
+    char *temp =t;
+    while(*s)
+    {
+        *t=*s;
+        ++t;
+        s+=2;
+        if(*t == '\0')
+            continue;
+    }
+    return temp;
+}
+
+int main(void) {
+    char *dest= calloc(50,sizeof(char));
+    char napis[] = "kocham uwm";
+    strcpyEven(dest,napis);
+    printf("%s",dest);
+}
